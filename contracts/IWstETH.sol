9ef0ef33b195212b40c9f6c14d36e1b8d1476efd
Emanuel Tesař <e.tesarr@gmail.com>
2025-05-20 10:30:00 +0200

    Prepare contracts for deployment

---
+++ contracts/IWstETH.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+/// @title A minimal interface for the wstETH contract indended to read the
+/// stETH value per token value of 1 unit of wstETH.
+interface IWstETH {
+    /// @notice Returns the stETH value per token value of 1 unit of wstETH.
+    function stEthPerToken() external view returns (uint256);
+}
