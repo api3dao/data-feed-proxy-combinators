6d4bf338b0b4eaf37f29a3dc63913adb525e852e
Emanuel Tesař <e.tesarr@gmail.com>
2025-05-21 16:41:04 +0200

    Address review comments

--- contracts/interfaces/IWstETH.sol
+++ contracts/interfaces/IWstETH.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.27;
 
 /// @title A minimal interface for the wstETH contract on Ethereum, used to read
 /// the stETH value per token value of 1 unit of wstETH.
+/// @dev The returned value is scaled to 18 decimals.
 interface IWstETH {
-    /// @notice Returns the stETH value per token value of 1 unit of wstETH.
+    /// @return The stETH value per token value of 1 unit of wstETH, scaled to 18 decimals.
     function stEthPerToken() external view returns (uint256);
 }
